baab553fc447e5a19040f8c061cecbd43504ce76
esketch <32926194+esketch-ai@users.noreply.github.com>
2023-04-24 13:03:00 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -27,7 +27,7 @@ export default siteConfig({
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon:
-    '/pages/android-chrome-192x192.png',
+    './pages/android-chrome-192x192.png',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
